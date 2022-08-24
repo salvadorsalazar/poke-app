da9a137c0fb56c60eeb8caf39952b32a537cb1e8
salvador salazar <salvador.g.salazar79@gmail.com>
2022-08-24 15:01:44 -0500

    style changes

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -38,41 +38,44 @@ const Main = () => {
   }, [url]);
   return (
     <>
-      <div className="container">
-        <div className="left-content">
-          <Card
-            pokemon={pokeData}
-            loading={loading}
-            infoPokemon={(poke) => setPokeDex(poke)}
-          />
+          <div className="container">
+              
+                        <div className="left-content">
+                        <Card
+                            pokemon={pokeData}
+                            loading={loading}
+                            infoPokemon={(poke) => setPokeDex(poke)}
+                        />
 
-          <div className="btn-group">
-            {prevUrl && (
-              <button
-                onClick={() => {
-                  setPokeData([]);
-                  setUrl(prevUrl);
-                }}
-              >
-                Previous
-              </button>
-            )}
+                        <div className="btn-group">
+                            {prevUrl && (
+                            <button
+                                onClick={() => {
+                                setPokeData([]);
+                                setUrl(prevUrl);
+                                }}
+                            >
+                                Previous
+                            </button>
+                            )}
 
-            {nextUrl && (
-              <button
-                onClick={() => {
-                  setPokeData([]);
-                  setUrl(nextUrl);
-                }}
-              >
-                Next
-              </button>
-            )}
-          </div>
-        </div>
+                            {nextUrl && (
+                            <button
+                                onClick={() => {
+                                setPokeData([]);
+                                setUrl(nextUrl);
+                                }}
+                            >
+                                Next
+                            </button>
+                            )}
+                        </div>
+                            </div>
+              
         <div className="right-content">
           <Pokeinfo data={pokeDex} />
-        </div>
+              </div>
+              
       </div>
     </>
   );
